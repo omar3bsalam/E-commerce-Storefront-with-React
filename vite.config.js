ea561dc9cb399014ac3bdbbd9a37964877f7b00b
omar3bsalam <omar3bsalam@gmail.com>
2025-11-26 21:58:54 +0200

    Empty repository

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/E-commerce-Storefront-with-React/',
-})
